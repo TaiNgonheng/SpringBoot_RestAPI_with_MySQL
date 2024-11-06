d68166cff694e02f4d0bd6a32d2076080f285e77
TaiNgonheng <heng68807@gmail.com>
2024-11-06 21:18:36 +0700

    Initial commit

---
+++ Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/TaiNgonhengApplication.java
@@ -0,0 +1,13 @@
+package To_do_Fullstack.TaiNgonheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiNgonhengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiNgonhengApplication.class, args);
+	}
+
+}
---
+++ Todo_Fullstack/src/test/java/To_do_Fullstack/TaiNgonheng/TaiNgonhengApplicationTests.java
@@ -0,0 +1,13 @@
+package To_do_Fullstack.TaiNgonheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiNgonhengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
