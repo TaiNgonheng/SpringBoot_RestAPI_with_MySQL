94402bdfb450ee9ed5acc0632f355315cef6958b
TaiNgonheng <heng68807@gmail.com>
2024-11-08 13:31:29 +0700

    spring full

--- Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/controller/TodoController.java
+++ Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/controller/TodoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("api/todos")
 @AllArgsConstructor
 public class TodoController {
+
     private TodoService todoService;
     // build for rest api
     @PostMapping
