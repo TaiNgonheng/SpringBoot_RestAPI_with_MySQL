2583206bdb7a1d26cc15394c52ea096cad7a407f
TaiNgonheng <heng68807@gmail.com>
2024-11-10 14:19:49 +0700

    Authentication

---
+++ Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/service/AuthService.java
@@ -0,0 +1,7 @@
+package To_do_Fullstack.TaiNgonheng.service;
+
+import To_do_Fullstack.TaiNgonheng.dto.RegisterDto;
+
+public interface AuthService{
+    String register(RegisterDto registerDto);
+}
