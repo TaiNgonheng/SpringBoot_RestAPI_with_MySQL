1b41411ad9e1a2f9fc98339220a7a09c797324ad
TaiNgonheng <heng68807@gmail.com>
2024-11-11 15:11:20 +0700

    set crossorigin to access from all url

--- Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/controller/AuthController.java
+++ Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/controller/AuthController.java
@@ -1,15 +1,14 @@
 package To_do_Fullstack.TaiNgonheng.controller;
 
+import To_do_Fullstack.TaiNgonheng.dto.LoginDto;
 import To_do_Fullstack.TaiNgonheng.dto.RegisterDto;
 import To_do_Fullstack.TaiNgonheng.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
@@ -18,9 +17,19 @@ public class AuthController {
     private AuthService authService;
 
     //Build register Rest api
+    //http://localhost:8080/api/auth/register
+    //follow RegisterDto
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
         String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    // login to user account
+    // http://localhost:8080/api/auth/login
+    // fill by LoginDto
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        String response = authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
