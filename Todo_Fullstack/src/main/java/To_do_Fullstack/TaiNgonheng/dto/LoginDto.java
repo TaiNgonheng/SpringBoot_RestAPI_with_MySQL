14d936adb312fadecb5199ab48ff72c5fa9c62d9
TaiNgonheng <heng68807@gmail.com>
2024-11-11 15:09:33 +0700

    Config Login Dto to accessible with login ui

---
+++ Todo_Fullstack/src/main/java/To_do_Fullstack/TaiNgonheng/dto/LoginDto.java
@@ -0,0 +1,17 @@
+package To_do_Fullstack.TaiNgonheng.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+
+}
